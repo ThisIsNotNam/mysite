9ddd056b61a84493fb5e7942f33860ee5979d47c
hoangthaiminh <minhhoangthai36@gmail.com>
2025-09-11 11:17:50 +0700

    Updated

--- tkb/views.py
+++ tkb/views.py
@@ -6,5 +6,10 @@ from django.shortcuts import render
 def tkbView(request):
     mtx=TKBapi.getTKB()
     today=datetime.date.today().weekday()
-    return render(request, 'TKB.html', {'mtx':mtx, 'day':today})
+    nextDay=(2 if (datetime.datetime.now().hour < 6 or (datetime.datetime.now().hour == 6 and datetime.datetime.now().minute <= 45)) else 3)
+    if(today + nextDay > 8):
+        today=2
+        nextDay=0
+    # print(datetime.datetime.now().hour)
+    return render(request, 'TKB.html', {'mtx':mtx, 'day':today + nextDay})
 
